da24fe258969fc62a288022ff2d092e1be350305
史鑫鑫 <xinxin.shi@bilintechnology.com>
2015-07-10 16:45:38 +0800

    driver pool set

--- jstest/settings.py
+++ jstest/settings.py
@@ -27,7 +27,7 @@ DOWNLOAD_HANDLERS = {
 # SCREEN_SHOT_SAVE_PATH = '.'
 
 # driver pool size
-DRIVER_POOL_SIZE = 4
+# DRIVER_POOL_SIZE = 4
 
 USER_AGENTS = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
